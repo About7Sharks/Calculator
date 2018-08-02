e11938f63de9bdb42a875aca167c11943c5c97dd
zac <zacarlin@gamil.com>
2018-08-02 10:40:20 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+ <calculator/>
+  </section>
+</template>
+
+<script>
+import Calculator from '~/components/Calculator.vue'
+
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+
+</style>
+
